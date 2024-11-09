74efb981ffd3a46b841533c73de2a9459d9ea023
logmi36 <158344474+logmi36@users.noreply.github.com>
2024-11-09 00:19:28 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace s13p05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_dibujar = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.btn_solucionar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_dibujar
+            // 
+            this.btn_dibujar.Location = new System.Drawing.Point(387, 670);
+            this.btn_dibujar.Name = "btn_dibujar";
+            this.btn_dibujar.Size = new System.Drawing.Size(75, 43);
+            this.btn_dibujar.TabIndex = 0;
+            this.btn_dibujar.Text = "Dibujar";
+            this.btn_dibujar.UseVisualStyleBackColor = true;
+            this.btn_dibujar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.Location = new System.Drawing.Point(625, 670);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(84, 43);
+            this.btn_cerrar.TabIndex = 1;
+            this.btn_cerrar.Text = "Cerrar";
+            this.btn_cerrar.UseVisualStyleBackColor = true;
+            this.btn_cerrar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_solucionar
+            // 
+            this.btn_solucionar.Location = new System.Drawing.Point(505, 670);
+            this.btn_solucionar.Name = "btn_solucionar";
+            this.btn_solucionar.Size = new System.Drawing.Size(83, 43);
+            this.btn_solucionar.TabIndex = 2;
+            this.btn_solucionar.Text = "Resolver";
+            this.btn_solucionar.UseVisualStyleBackColor = true;
+            this.btn_solucionar.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(706, 639);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 660);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "largo";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(102, 660);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 700);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "pasos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 700);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 726);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_solucionar);
+            this.Controls.Add(this.btn_cerrar);
+            this.Controls.Add(this.btn_dibujar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Semana 13";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_dibujar;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.Button btn_solucionar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace s13p05
+{
+    public partial class Form1 : Form
+    {
+
+        public static readonly Color[] colores = { Color.SkyBlue, Color.White };
+        public static List<Nodo> nodos;
+        public static int[,] laberinto;
+        public Grafo grafo;
+        public static Dictionary<string, int> dic;
+        List<int> ruta;
+        public static int pos;
+
+        public int largo;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nodos = new List<Nodo>();
+            btn_solucionar.Enabled = false;
+            timer1.Interval = 80;
+
+            largo = 6;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Escape) {
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DibujarLaberinto();
+        }
+
+        private void DibujarLaberinto() {
+
+            
+            largo = int.Parse(numericUpDown1.Text);
+
+            btn_solucionar.Enabled = true;
+            pos = 0;
+            timer1.Enabled = false;
+            panel1.Controls.Clear();
+            nodos = new List<Nodo>();
+            laberinto = new int[largo, largo];
+
+            label3.Text = pos.ToString();
+
+            dic = new Dictionary<string, int>();
+
+            int k = 0;
+
+            int f0 = 0;
+            int c0 = 0;
+
+            Random rnd = new Random();
+
+            int ancho = panel1.Width / largo;
+            int alto = panel1.Height / largo;
+
+            for (int i = 0; i < largo; i++)
+            {
+                for (int j = 0; j < largo; j++)
+                {
+                    int f1 = f0 + ancho * i;
+                    int c1 = c0 + alto * j;
+                    int cl = rnd.Next(0, 2);
+
+                    if (i == 0 && j == 0)
+                        cl = 1;
+                    if (i == largo-1 && j == largo-1)
+                        cl = 1;
+
+                    PictureBox picture = new PictureBox();
+                    panel1.Controls.Add(picture);
+                    picture.Width = ancho;
+                    picture.Height = alto;
+                    picture.Location = new Point(f1, c1);
+                    picture.BackColor = colores[cl];
+                    picture.Name = "pic_" + i + "_" + j;
+                    picture.Paint += picture_paint;
+                    picture.MouseUp += (s, args) =>
+                    {
+                        if (args.Button == MouseButtons.Left)
+                        {
+                            picture_Click_left(s, args);
+                        }
+                        if (args.Button == MouseButtons.Right)
+                        {
+                            picture_Click_rigth(s, args);
+                        }
+                    };
+
+                    laberinto[i, j] = cl;
+
+                    Nodo nodo = new Nodo();
+                    nodo.nombre = i + "," + j;
+                    nodo.dato = k;
+                    nodo.fila = i;
+                    nodo.columna = j;
+                    nodos.Add(nodo);
+                    dic[nodo.nombre] = k;
+                    k = k + 1;
+                }
+            }
+        }
+
+        private void MostrarLaberinto() {
+           // Console.WriteLine("==============================================");
+           // Console.WriteLine();
+            for (int i = 0; i < largo; i++)
+            {
+                Console.Write("\t{0}", i);
+            }
+           // Console.WriteLine();
+            for (int i = 0; i < largo; i++)
+            {
+                Console.Write(i);
+                for (int j = 0; j < largo; j++)
+                {
+                    Console.Write("\t{0}", laberinto[j,i]);
+                }
+               // Console.WriteLine();
+            }
+
+           // Console.WriteLine("==============================================");
+        }
+
+        private void CalcularMAdj() {
+
+            int nodo = 0;
+            int inicio = 0;
+            int final = (largo* largo)-1;
+            int distancia = 0;
+            int n = 0;
+            int m = 0;
+            int cantNodos = largo* largo;
+
+            grafo = new Grafo(largo* largo);
+
+           // Console.WriteLine("==============================================");
+
+            for (int j = 0; j < largo; j++) {
+                for (int i = 0; i < largo-1; i++)
+                {
+                    if((laberinto[i,j]==1) && (laberinto[i+1,j]==1))
+                    {
+                        //Console.WriteLine("{0}\t{1}\t{2}", i, j, laberinto[i,j]);
+                        string nombre = j+","+i;
+                        int nodo1 = dic[nombre];
+                        nombre = j+","+ (i + 1);
+                        int nodo2 = dic[nombre];
+
+                        grafo.AdicionarArista(nodo1, nodo2);
+                        grafo.AdicionarArista(nodo2, nodo1);
+                    }
+                }
+            }
+
+            for (int i = 0; i < largo; i++)
+            {
+                for (int j = 0; j < largo-1; j++)
+                {
+                    if ((laberinto[i, j] == 1) && (laberinto[i, j+1] == 1))
+                    {
+                        //Console.WriteLine("{0}\t{1}\t{2}", i, j, laberinto[i, j]);
+                        string nombre = j+","+i;
+                        int nodo1 = dic[nombre];
+                        nombre = (j+1)+","+i;
+                        int nodo2 = dic[nombre];
+
+                        grafo.AdicionarArista(nodo1, nodo2);
+                        grafo.AdicionarArista(nodo2, nodo1);
+                    }
+                }
+            }
+
+
+
+            //for (int j = 0; j < 6; j++)
+            //{
+            //    for (int i = 5; i > 0; i--)
+            //    {
+            //        if ((laberinto[i, j] == 1) && (laberinto[i - 1, j] == 1))
+            //        {
+            //           // Console.WriteLine("{0}\t{1}\t{2}", i, j, laberinto[i,j]);
+            //            string nombre = j + "," + i;
+            //            int nodo1 = dic[nombre];
+            //            nombre = j + "," + (i + 1);
+            //            int nodo2 = dic[nombre];
+
+            //            grafo.AdicionarArista(nodo1, nodo2);
+            //        }
+            //    }
+            //}
+
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    for (int j = 5; j > 0; j--)
+            //    {
+            //        if ((laberinto[i, j] == 1) && (laberinto[i, j - 1] == 1))
+            //        {
+            //           // Console.WriteLine("{0}\t{1}\t{2}", i, j, laberinto[i, j]);
+            //            string nombre = j + "," + i;
+            //            int nodo1 = dic[nombre];
+            //            nombre = (j + 1) + "," + i;
+            //            int nodo2 = dic[nombre];
+
+            //            grafo.AdicionarArista(nodo1, nodo2);
+            //        }
+            //    }
+            //}
+
+
+
+            //grafo.MostrarAdyacencia();
+
+           // Console.WriteLine("==============================================================");
+           // Console.WriteLine("inicio\t:\t{0}", inicio);
+           // Console.WriteLine("final\t:\t{0}", final);
+           // Console.WriteLine("==============================================================");
+
+           // Console.WriteLine();
+
+            int[,] tabla;
+            tabla = new int[cantNodos, 3];
+
+            //0 visitado
+            //1 distancia
+            //2 previo
+
+            for (n = 0; n < cantNodos; n++)
+            {
+                tabla[n, 0] = 0;
+                tabla[n, 1] = int.MaxValue;
+                tabla[n, 2] = 0;
+            }
+
+            tabla[inicio, 1] = 0;
+
+            //MostrarTabla(tabla);
+
+           // Console.WriteLine("==============================================================");
+
+
+            for (distancia = 0; distancia < cantNodos; distancia++)
+            {
+                for (n = 0; n < cantNodos; n++)
+                {
+                    if ((tabla[n, 0] == 0) && (tabla[n, 1] == distancia))
+                    {
+                        tabla[n, 0] = 1;
+                        for (m = 0; m < cantNodos; m++)
+                        {
+                            if (grafo.ObtenerAdyacencia(n, m) == 1)
+                            {
+                                if (tabla[m, 1] == int.MaxValue)
+                                {
+                                    tabla[m, 1] = distancia + 1;
+                                    tabla[m, 2] = n;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //MostrarTabla(tabla);
+
+           // Console.WriteLine("==============================================================");
+
+            //obtener la ruta
+
+            ruta = new List<int>();
+
+            nodo = final;
+
+            while (nodo != inicio)
+            {
+                ruta.Add(nodo);
+                nodo = tabla[nodo, 2];
+            }
+
+            ruta.Add(inicio);
+            ruta.Reverse();
+
+            //foreach (int pos in ruta)
+            //{
+            //    Console.Write("{0}\t", pos);
+            //}
+
+           // Console.WriteLine();
+           // Console.WriteLine("==============================================================");
+
+            if (ruta.Count > 2)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                btn_dibujar.Enabled = true;
+            }
+            
+        }
+
+
+
+        public static void MostrarTabla(int[,] tabla)
+        {
+            for (int i = 0; i < tabla.GetLength(0); i++)
+            {
+               // Console.WriteLine("{0} --> {1}\t{2}\t{3}", i, tabla[i, 0], tabla[i, 1], tabla[i, 2]);
+            }
+           // Console.WriteLine();
+        }
+
+
+
+        void picture_Click_rigth(object sender, EventArgs e)
+        {
+
+        }
+
+            void picture_Click_left(object sender, EventArgs e)
+        {
+
+            foreach (PictureBox panel in panel1.Controls) {
+                if (panel.BackColor == Color.Violet) {
+                    panel.BackColor = Color.White;
+                }
+            }
+
+            label3.Text = "0";
+
+            btn_solucionar.Enabled = true;
+            PictureBox picture = (PictureBox)sender;
+            string nombre = picture.Name;
+            int i = Convert.ToInt32(nombre.Split('_')[1]);
+            int j = Convert.ToInt32(nombre.Split('_')[2]);
+
+            if ((i == 0) && (j == 0))
+                return;
+            if ((i == largo-1) && (j == largo-1))
+                return;
+
+            if (picture.BackColor == Color.SkyBlue) {
+                picture.BackColor = Color.White;
+                laberinto[i, j] = 1;
+            }
+            else
+            {
+                picture.BackColor = Color.SkyBlue;
+                laberinto[i, j] = 0;
+            }
+        }
+
+        void picture_paint(object sender, PaintEventArgs e)
+        {
+            PictureBox picture = (PictureBox)sender;
+            string nombre = picture.Name;
+            int i = Convert.ToInt32(nombre.Split('_')[1]);
+            int j = Convert.ToInt32(nombre.Split('_')[2]);
+
+            ControlPaint.DrawBorder(e.Graphics, picture.ClientRectangle, Color.Black, ButtonBorderStyle.Solid);
+
+            if (i == 0 && j == 0) {
+                e.Graphics.DrawEllipse( new Pen(Color.Red, 4f), picture.Size.Width / 2-10, picture.Size.Height/2-10, picture.Size.Width/2, picture.Size.Height/2);
+            }
+            if (i == largo-1 && j == largo-1)
+            {
+                e.Graphics.DrawEllipse(new Pen(Color.Red, 4f), picture.Size.Width / 2 - 10, picture.Size.Height / 2 - 10, picture.Size.Width / 2, picture.Size.Height / 2);
+            }
+
+            //picture.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            btn_solucionar.Enabled = false;
+            btn_dibujar.Enabled = false;
+            label3.Text = "0";
+
+            //MostrarLaberinto();
+            CalcularMAdj();
+        }
+
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // Console.WriteLine("{0}\t{1}",pos, ruta[pos]);
+            
+            int nodo = ruta[pos];
+            int i = nodos[nodo].fila;
+            int j = nodos[nodo].columna;
+            string nombre= "pic_" + j + "_" + i;
+
+            foreach (PictureBox picture in panel1.Controls) {
+                if (picture.Name == nombre) {
+                    picture.BackColor = Color.Violet;
+                }
+            }
+
+            pos = pos + 1;
+
+            label3.Text = pos.ToString();
+
+            if (pos == ruta.Count) {
+                timer1.Enabled = false;
+                pos = 0;
+                btn_dibujar.Enabled = true;
+                
+            }
+
+            
+                
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
